0d92fbf08418000ba974f155e64af2e486bec74d
goeunj <goeunj@sfu.ca>
2025-05-28 16:13:04 -0700

    Code Cleanup

--- Dishes/src/game/game.js
+++ Dishes/src/game/game.js
@@ -3,75 +3,61 @@ const numRows = 3, numCols = 3;
 
 let result = document.querySelector('p');
 let text = document.querySelector('h2');
-let winner = false, gameEnd = false;
+let winner = {value: false};
 
-let player1 = {value: true}, player2 = {value: false};
-let player1Count = {value: 0}, player2Count = {value: 0};
+function updateCounter(mtr, i, j, zeroCounter, oneCounter, twoCounter){
+    //loops through matrix to count clicks at each row index
+    if(mtr[i][j] == 0){
+        zeroCounter.value++;
+    }else if(mtr[i][j] == 1){
+        oneCounter.value++;
+    }else if(mtr[i][j] == 2){
+        twoCounter.value++;
+    }
+}
 
-let player1Btn = Array(5).fill(null).map(() => Array(2).fill(null));
-let player2Btn = Array(5).fill(null).map(() => Array(2).fill(null));
-
-function checkWinner(mtr, text, player){
+function checkWinner(mtr){
     //variables to count number of row clicks at each index
-    let count_row_zero = 0, count_row_one = 0, count_row_two = 0;
-    
+    let count_row_zero = {value: 0}, count_row_one = {value: 0}, count_row_two = {value: 0};
     //variables to count number of col clicks at each index
-    let count_col_zero = 0, count_col_one = 0, count_col_two = 0;
+    let count_col_zero = {value: 0}, count_col_one = {value: 0}, count_col_two = {value: 0};
 
     // variables to determine if {[0,0], [1,1], [2,2]} has been selected
     let middle = false, bothOne = false, bothTwo = false;
+    let specialCount = 0;
 
     for(let i = 0; i < mtr.length; i ++){
         // checks if [0, 0], [1, 1] or [2, 2] were clicked on
         if(mtr[i].includes(1) && (!mtr[i].includes(0) && !mtr[i].includes(2))){
             middle = true;
-            // console.log("middle clicked");
         }else if(mtr[i].includes(0) && (!mtr[i].includes(1) && !mtr[i].includes(2))){
             bothOne = true;
         }else if(mtr[i].includes(2) && (!mtr[i].includes(1) && !mtr[i].includes(0))){
             bothTwo = true;
+        }else if(mtr[i].includes(2) && mtr[i].includes(0) && !mtr.includes(1)){
+            // checks if [0, 2] and [0, 2] has been clicked
+            specialCount++;
         }
 
         //loops through matrix to count clicks at each row index
-        if(mtr[i][0] == 0){
-            count_row_zero++;
-            // console.log("count zero: " + count_row_zero);
-        }else if(mtr[i][0] == 1){
-            count_row_one++;
-            // console.log("one " + count_row_one + " " + i[0]);
-        }else if(mtr[i][0] == 2){
-            count_row_two++;
-            // console.log("two: " + count_row_two);
-        }
+        updateCounter(mtr, i, 0, count_row_zero, count_row_one, count_row_two);
 
         //loops through matrix to count clicks at each col index
-        if(mtr[i][1] == 0){
-            count_col_zero++;
-            // console.log("count col zero: " + count_col_zero);
-        }else if(mtr[i][1] == 1){
-            count_col_one++;
-            // console.log("col one " + count_col_one + " " + i[0]);
-        }else if(mtr[i][1] == 2){
-            count_col_two++;
-            // console.log("col two: " + count_col_two);
-        }
+        updateCounter(mtr, i, 1, count_col_zero, count_col_one, count_col_two);
     }
 
     //straight col win; identical x value need a count of 3 to determine col win
-    if(count_row_zero == 3 || count_row_one == 3 || count_row_two == 3){
-        printResult(player, text);
+    if(count_row_zero.value == 3 || count_row_one.value == 3 || count_row_two.value == 3){
         return true;
     }
     //straight row win; identical y value need a count of 3 to determine row win
-    else if(count_col_zero == 3 || count_col_one == 3 || count_col_two == 3){
-        printResult(player, text);
+    else if(count_col_zero.value == 3 || count_col_one.value == 3 || count_col_two.value == 3){
         return true;
     }
     //diagonal win
-    // middle val needs to be clicked and either [1,1] and [2,2] or count for all row/col values need to equal 1
-    else if(middle &&((bothOne && bothTwo) || (count_col_one == 1 && count_col_two == 1 && count_col_zero == 1) &&
-        (count_row_one == 1 && count_row_zero == 1 && count_row_two == 1))){
-        printResult(player, text);
+    // middle val needs to be clicked and either [1,1] and [2,2] or 
+    // middle val clicked and [0, 2] or [2, 0] clicked and ensure count for col one and col two are both greater than 0 
+    else if(middle &&((bothOne && bothTwo) || specialCount == 2)){
         return true;
     }
 
@@ -85,10 +71,11 @@ function setButton(){
     // show hidden button
     tryAgain.style.display = "inline-block";
 
-    if(winner){
-        result.textContent = "It's a tie! Try again.";
-    }else{
+    if(winner.value){
         tryAgain.textContent = "PLAY AGAIN";
+    }else{
+        result.textContent = "It's a tie! Try again.";
+        tryAgain.textContent = "TRY AGAIN";
     }
 
     // reload game if button clicked
@@ -98,7 +85,7 @@ function setButton(){
 }
 
 //prints results 
-function printResult(player, text){
+function printResult(player){
     if(player == 2){
         result.textContent = "PLAYER 1 IS DOING THE DISHES!";
     }else{
@@ -133,10 +120,19 @@ function gamePlay(player, playerBtn, playerCount, i, j){
     }
     // check winner
     if(playerCount.value >= 2){
-        winner = checkWinner(playerBtn, text, player);
+        winner.value = checkWinner(playerBtn);
+        if(winner.value){
+            printResult(player);
+        }
     }
     playerCount.value++;
 }
+
+let player1 = {value: true}, player2 = {value: false};
+let player1Count = {value: 0}, player2Count = {value: 0};
+
+let player1Btn = Array(5).fill(null).map(() => Array(2).fill(null));
+let player2Btn = Array(5).fill(null).map(() => Array(2).fill(null));
 
 //create table of buttons in table body
 for (let i = 0; i < numRows; i++) {
@@ -150,7 +146,7 @@ for (let i = 0; i < numRows; i++) {
         btn.classList.add("btn");
 
         btn.addEventListener('click', function() {
-            if(!winner){
+            if(!winner.value){
                 result.textContent = "Player 2's turn";
                 // player 1 goes first; if player1 clicks, check with O
                 if(player1.value){
@@ -159,7 +155,7 @@ for (let i = 0; i < numRows; i++) {
                     //keep track of whose turn
                     gamePlay(1, player1Btn, player1Count, i, j);
                 }else{
-                    result.textContent = "Player 1's turn.";
+                    result.textContent = "Player 1's turn";
                     btn.textContent = "X";
 
                     // keep track of whose turn
@@ -167,12 +163,11 @@ for (let i = 0; i < numRows; i++) {
                 }
 
                 // check if no winner yet and if player 1 has used up all their peices to identify tie
-                if(!winner && (player1Count == 5)){
+                if(!winner.value && (player1Count.value == 5)){
                     setButton();
                 }
                 btn.disabled = "disabled";
             }else{
-                // console.log("disable " + winner);
                 btn.disabled = "disabled";
             }
         });
